71b43649e74531f25dad16cac824361643ce7c72
EvgeniyWis <KworkEvgeniy@yandex.ru>
2025-04-28 20:56:06 +0300

    Add writing folder name.

--- bot/handlers/hand_start.py
+++ bot/handlers/hand_start.py
@@ -5,6 +5,7 @@ from aiogram.fsm.context import FSMContext
 from utils import text
 from states import UserState
 from utils.generateImages import generateImages
+from logger import logger
 
 
 # Отправка стартового меню при вводе "/start"
@@ -24,16 +25,24 @@ async def write_prompt(message: types.Message, state: FSMContext):
         if image:
             await message_for_edit.delete()
             await message.answer(text.GENERATE_IMAGE_SUCCESS_TEXT)
+            await state.set_state(UserState.write_folder_name)
 
     except Exception as e:
         await message.answer("Произошла ошибка при генерации изображения")
-        print(f"Ошибка: {e}")
+        await state.clear()
+        logger.error(f"Произошла ошибка при генерации изображения: {e}")
+
+
+# Обработка ввода названия папки
+async def write_folder_name(message: types.Message, state: FSMContext):
+    folder_name = message.text
 
     await state.clear()
 
-
+# Добавление обработчиков
 def hand_add():
     router.message.register(start, StateFilter("*"), CommandStart())
 
     router.message.register(write_prompt, StateFilter(UserState.write_prompt))
 
+    router.message.register(write_folder_name, StateFilter(UserState.write_folder_name))
--- bot/states/UserState.py
+++ bot/states/UserState.py
@@ -2,3 +2,4 @@ from aiogram.fsm.state import State, StatesGroup
 
 class UserState(StatesGroup):
     write_prompt = State()
+    write_folder_name = State()
