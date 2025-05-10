4011df5f41c5608dbf8e92325fb3fd2bb9ae879d
EvgeniyWis <KworkEvgeniy@yandex.ru>
2025-05-10 19:54:06 +0300

    Add settings 2 and 3.

--- bot/utils/generateImages/dataArray/setting_1/generateLoras.py
+++ bot/utils/generateImages/dataArray/setting_1/generateLoras.py
@@ -3,12 +3,12 @@ from ..generateLoras import generateLoras
 # Функция для генерации массива лор для настройки 1
 def setting1_generateLoras(weights: list[int]):
     loras = ["Pony_Realism_Slider.safetensors", "Breast_Size_Slider.safetensors", 
-    "Nipple_Size_Slider_alpha1.0_rank4_noxattn_last.safetensors",
-    "body_weight_slider_v1.safetensors",
-    "StS-Breast-Enlarger-Slider-PonyXL-v0.8.safetensors",
-    "igbaddie-PN.safetensors",
-    "natural_breasts_v1.safetensors",
-    "Hour_Glass_Body_By_Stable_Yogi_PONY0_V1.safetensors",
-    "lightingSlider.safetensors"]
+    "Nipple_Size_Slider.safetensors",
+    "Body_Weight_Slider.safetensors",
+    "Breast_Enlargement_Slider.safetensors",
+    "Crazy_Girlfriend_Mix.safetensors",
+    "Big_Natural_Breasts.safetensors",
+    "Hour_Glass_Body_By_Stable_Yogi.safetensors",
+    "lighting_darkness_slider.safetensors"]
     
     return generateLoras(weights, loras)
