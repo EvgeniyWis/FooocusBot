652e045e798aabb4d6f8cc65ce4a24d5f0377974
EvgeniyWis <KworkEvgeniy@yandex.ru>
2025-05-14 10:45:23 +0300

    Finish test video generation.

--- bot/keyboards/user/buttons.py
+++ bot/keyboards/user/buttons.py
@@ -1,7 +1,7 @@
 from aiogram.types import InlineKeyboardButton
 
 # Функция для выдавания кнопок с выбором типа генерации
-def getGenerationsAmountButtons(prefix: str):
+def getGenerationsAmountButtons(prefix: str, with_test_generation: bool = True):
     inline_buttons = [
         [InlineKeyboardButton(text='⚙️ Тестовая генерация', callback_data=f'{prefix}|test')],
         [InlineKeyboardButton(text=f'⚡️ Рабочая генерация', callback_data=f'{prefix}|all')]
