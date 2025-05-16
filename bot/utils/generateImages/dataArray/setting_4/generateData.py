faee491371965a408d82611b9f296bdeb99200b6
EvgeniyWis <KworkEvgeniy@yandex.ru>
2025-05-16 13:45:40 +0300

    bugfix data

--- bot/utils/generateImages/dataArray/setting_4/generateData.py
+++ bot/utils/generateImages/dataArray/setting_4/generateData.py
@@ -4,7 +4,7 @@ from ..generateData import generateData
 # Функция для генерации данных для запроса настройки 4
 def setting4_generateData(model_name: str, picture_folder_id: str, video_folder_id: str, prompt: str, lorasWeights: list[int]):
     loras = setting4_generateLoras(lorasWeights)
-    advanced_params = {"guidance_scale": 4, "sampler_name": "euler a", "overwrite_step": 35}
+    advanced_params = {"guidance_scale": 4, "sampler_name": "euler_ancestral", "overwrite_step": 35}
     base_config_model_name = "CyberIllustrious_CyberRealistic.safetensors"
     data = generateData(model_name, picture_folder_id, video_folder_id, prompt, loras, advanced_params, base_config_model_name)
     return data
--- bot/utils/generateImages/upscaleImage.py
+++ bot/utils/generateImages/upscaleImage.py
@@ -1,16 +1,20 @@
 from ..jobs.getJobID import getJobID
 from ..jobs.checkJobStatus import checkJobStatus
-
+from logger import logger
 
 # Функция для upscale сгенерированного изображения
 async def upscaleImage(input_image: str) -> str:
+    # Логирование
+    logger.info(f"Делаем upscale для изображения...")
+
     # Формируем json для отправки
     dataJSON = {
         "input": {
             "api_name": "upscale-vary2",
             "require_base64": True,
             "uov_method": "Upscale (1.5x)",
-            "input_image": input_image
+            "input_image": input_image,
+            "advanced_params": {"guidance_scale": 3.5, "sampler_name": "euler_ancestral", "overwrite_step": 30},
         }
     }
 
