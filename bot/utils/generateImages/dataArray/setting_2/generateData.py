b5ccc91b6fc3ca1c74c039051943293662df9f15
EvgeniyWis <KworkEvgeniy@yandex.ru>
2025-05-18 17:02:19 +0300

    change steps amount

--- bot/utils/generateImages/dataArray/setting_2/generateData.py
+++ bot/utils/generateImages/dataArray/setting_2/generateData.py
@@ -4,7 +4,7 @@ from ..generateData import generateData
 # Функция для генерации данных для запроса настройки 2
 def setting2_generateData(model_name: str, picture_folder_id: str, video_folder_id: str, prompt: str, lorasWeights: list[int]):
     loras = setting2_generateLoras(lorasWeights)
-    advanced_params = {"guidance_scale": 4, "sampler_name": "euler_ancestral", "overwrite_step": 35}
+    advanced_params = {"guidance_scale": 4, "sampler_name": "euler_ancestral", "overwrite_step": 60}
     base_config_model_name = "CyberIllustrious_CyberRealistic.safetensors"
     data = generateData(model_name, picture_folder_id, video_folder_id, prompt, loras, advanced_params, base_config_model_name)
     return data
