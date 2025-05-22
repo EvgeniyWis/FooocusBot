fea4016c98d79b87e31ca3804d617b30bded62bb
EvgeniyWis <KworkEvgeniy@yandex.ru>
2025-05-22 21:24:12 +0300

    bugfix

--- bot/utils/handlers/editMessageOrAnswer.py
+++ bot/utils/handlers/editMessageOrAnswer.py
@@ -8,6 +8,5 @@ async def editMessageOrAnswer(
         await call.message.edit_text(text, reply_markup=reply_markup)
     except Exception as e:
         logger.error(f"Ошибка при редактировании сообщения: {e}")
-        await editMessageOrAnswer(
-        call,text, reply_markup=reply_markup)
+        await call.message.answer(text, reply_markup=reply_markup)
 
