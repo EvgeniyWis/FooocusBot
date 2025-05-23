4df9085842a002617ffe42a241409422de27ef3e
EvgeniyWis <KworkEvgeniy@yandex.ru>
2025-05-24 00:42:41 +0300

    Add possibility to change images number for models and change for some models to 8.

--- bot/utils/generateImages/dataArray/setting_4/generateData.py
+++ bot/utils/generateImages/dataArray/setting_4/generateData.py
@@ -2,8 +2,9 @@ from .generateLoras import setting4_generateLoras
 from ..generateData import generateData
 
 # Функция для генерации данных для запроса настройки 4
-def setting4_generateData(model_name: str, picture_folder_id: str, video_folder_id: str, prompt: str, lorasWeights: list[int]):
+def setting4_generateData(model_name: str, picture_folder_id: str, video_folder_id: str, prompt: str, 
+    lorasWeights: list[int], image_number: int = 4):
     loras = setting4_generateLoras(lorasWeights)
     base_config_model_name = "CyberIllustrious_CyberRealistic.safetensors"
-    data = generateData(model_name, picture_folder_id, video_folder_id, prompt, loras, base_config_model_name)
+    data = generateData(model_name, picture_folder_id, video_folder_id, prompt, loras, base_config_model_name, image_number=image_number)
     return data
