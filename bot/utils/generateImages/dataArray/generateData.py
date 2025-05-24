98ab6b49d2f82324c91c774249cd316ef9a8c608
EvgeniyWis <KworkEvgeniy@yandex.ru>
2025-05-24 14:12:08 +0300

    Made negative prompt for specific models.

--- bot/utils/generateImages/dataArray/generateData.py
+++ bot/utils/generateImages/dataArray/generateData.py
@@ -1,6 +1,7 @@
 # Глобальная функция для генерации данных для запроса
 def generateData(model_name: str, picture_folder_id: str, video_folder_id: str, prompt: str,
-    loras: list[dict], base_config_model_name: str, embeddings: list[str] = [], image_number: int = 4):
+    loras: list[dict], base_config_model_name: str, image_number: int,
+    negative_prompt: str, embeddings: list[str] = []):
     data = {"json": {
         "input": {
             "api_name": "txt2img",
@@ -9,7 +10,7 @@ def generateData(model_name: str, picture_folder_id: str, video_folder_id: str, 
             "loras": loras,
             "image_number": image_number,
             "advanced_params": {"sampler_name": "euler_ancestral", "overwrite_step": 60},
-            "negative_prompt": "score_6, score_5, score_4, Negative_&_Positive_Embeddings_By_Stable_Yogi, negative_hand, pony, negativeXL_D, low quality, oversaturated, disfigured, poorly, bad, wrong, mutated, worst quality, normal quality, ugly face, mutated hands, extra fingers, poorly drawn hands, fused fingers, too many fingers, long neck, bad hands, text, signature, signature artist, multiple female, multiple male, bad anatomy, low res, blurry face, blurry eyes, tiny hands, tiny feet, multiple women, disproportionately large head, disproportionately long torso, six fingers, low quality hands, hat, multicolored hair, pubic hair, asian, tan lines, makeup, lipstick, playing cards, black skin, steering wheel, man, naked nipples, naked breasts, cartoon, anime, 3d, cgi, illustration, doll-like, overly muscular, chubby, plastic skin, waxy texture, blurry, jpeg artifacts, extra limbs, distorted proportions, unnatural face, unrealistic anatomy, deformed eyes, exaggerated curves, barbie face, uncanny valley, big head, overexposed, underexposed, low-quality shading, unnatural smile",
+            "negative_prompt": negative_prompt,
             "base_model_name": base_config_model_name,
             "style_selections": [],
             "guidance_scale": 3.5,
