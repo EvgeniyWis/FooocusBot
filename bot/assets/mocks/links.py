d021952f22815ecb77bf0663b276a6d1e6618743
EvgeniyWis <KworkEvgeniy@yandex.ru>
2025-05-26 21:21:40 +0300

    Add mocks.

---
+++ bot/assets/mocks/links.py
@@ -0,0 +1,7 @@
+### Мок-ссылки
+
+# Мок-ссылка для сохранения изображений
+MOCK_LINK_FOR_SAVE_IMAGE = "https://drive.google.com/file/d/1dSkG6TsDFK3Tp0LlMDkTqyWZpsvnvvg0/view?usp=drivesdk"
+
+# Мок-ссылка для сохранения видео
+MOCK_LINK_FOR_SAVE_VIDEO = "https://drive.google.com/drive/folders/18V64itY-c07U43aZb09mdzgVU9UGa242"
