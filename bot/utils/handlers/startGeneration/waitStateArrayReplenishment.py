1b367ecaa13c3f986522fcd202e9ad6a49c9042d
EvgeniyWis <KworkEvgeniy@yandex.ru>
2025-05-27 21:08:25 +0300

    Modernize waitStateArrayReplenishment. Add regeneration for second step.

--- bot/utils/handlers/startGeneration/waitStateArrayReplenishment.py
+++ bot/utils/handlers/startGeneration/waitStateArrayReplenishment.py
@@ -6,20 +6,25 @@ import asyncio
 # Функция для ожидания пополнения массива в стейте
 async def waitStateArrayReplenishment(state: FSMContext, array_name: str, 
     stateNamesForCycleExit: tuple[str, str]) -> list[dict] | bool:
-    while True:
-        stateData = await state.get_data()
 
-        if not stateData[array_name]:
-            array = []
-        else:
-            array = stateData[array_name]
+    try:
+        while True:
+            stateData = await state.get_data()
 
-        logger.info(f"Модели для генерации {array}: {[list(i.keys())[0] for i in array]}")
+            if not stateData[array_name]:
+                array = []
+            else:
+                array = stateData[array_name]
 
-        if len(array) > 0:
-            return array
+            logger.info(f"Модели для генерации {array}: {[list(i.keys())[0] for i in array]}")
 
-        if stateData[stateNamesForCycleExit[0]] == stateData[stateNamesForCycleExit[1]]:
-            return False
+            if len(array) > 0:
+                return array
 
-        await asyncio.sleep(5)
+            if stateData[stateNamesForCycleExit[0]] == stateData[stateNamesForCycleExit[1]]:
+                return False
+
+            await asyncio.sleep(5)
+    except Exception as e:
+        logger.error(f"Ошибка в функции waitStateArrayReplenishment: {e}")
+        return False
