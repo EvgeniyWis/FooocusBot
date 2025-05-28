1c71de6c5351e2602b1e88d8709868ab763f50ca
EvgeniyWis <KworkEvgeniy@yandex.ru>
2025-05-28 19:28:00 +0300

    Fix video saving.

--- bot/utils/handlers/videoGeneration/saveVideo.py
+++ bot/utils/handlers/videoGeneration/saveVideo.py
@@ -2,18 +2,14 @@ from utils.saveImages.getFolderDataByID import getFolderDataByID
 from utils.files.saveFile import saveFile
 from utils import text
 from logger import logger
-from InstanceBot import bot
 from utils.generateImages.dataArray.getModelNameIndex import getModelNameIndex
-from utils.handlers.editMessageOrAnswer import editMessageOrAnswer
 from aiogram import types
 from datetime import datetime
-import asyncio
-import os
+from utils.generateImages.dataArray.getDataByModelName import getDataByModelName
 
 
 # Функция для сохранения видео в папку модели
-async def saveVideo(video_path: str, model_name: str, video_folder_id: str, 
-    message: types.Message):
+async def saveVideo(video_path: str, model_name: str, message: types.Message):
     # Получаем текущую дату
     now = datetime.now().strftime("%Y-%m-%d")
 
@@ -26,19 +22,22 @@ async def saveVideo(video_path: str, model_name: str, video_folder_id: str,
     # Отправляем сообщение о начале сохранения видео
     await message.answer(text.SAVE_VIDEO_PROGRESS_TEXT.format(model_name, model_name_index))
 
+    # Получаем данные о модели по имени
+    model_data = getDataByModelName(model_name)
+
     # Сохраняем видео
-    link = await saveFile(video_path, user_id, model_name, video_folder_id, now, False)
+    link = await saveFile(video_path, user_id, model_name, model_data['video_folder_id'], now, False)
 
     if not link:
         await message.answer(text.SAVE_FILE_ERROR_TEXT)
         return
     
     # Получаем данные родительской папки
-    folder = getFolderDataByID(video_folder_id)
+    folder = getFolderDataByID(model_data['video_folder_id'])
     parent_folder_id = folder['parents'][0]
     parent_folder = getFolderDataByID(parent_folder_id)
 
-    logger.info(f"Данные папки по id {video_folder_id}: {folder}")
+    logger.info(f"Данные папки по id {model_data['video_folder_id']}: {folder}")
 
     # Отправляем сообщение о сохранении видео
     await message.answer(text.SAVE_VIDEO_SUCCESS_TEXT
