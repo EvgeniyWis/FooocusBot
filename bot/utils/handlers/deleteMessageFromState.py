e27c1f996808c95a89c6fd4b13aa2498a5668c56
EvgeniyWis <KworkEvgeniy@yandex.ru>
2025-06-08 21:59:30 +0300

    Add messages deleting (progress messages, photo and video). Add getDataInDictsArray function for getting data in arrays of dicts. Also, add deleteMessageFromState for deleting message, which id is get from state. Add video for saving video message.

---
+++ bot/utils/handlers/deleteMessageFromState.py
@@ -0,0 +1,28 @@
+from aiogram.fsm.context import FSMContext
+from logger import logger
+
+from .getDataInDictsArray import getDataInDictsArray
+
+from InstanceBot import bot
+
+
+# Функция для удаления сообщения, полученного из стейта
+async def deleteMessageFromState(state: FSMContext, key: str, model_name: str, chat_id: int):
+    # Получаем сообщение из стейта
+    stateData = await state.get_data()
+    messages_ids = stateData.get(key, [])
+    logger.info(f"Стейт сообщений для модели {model_name} по ключу {key}: {messages_ids}")
+
+    data = await getDataInDictsArray(messages_ids, model_name)
+
+    # Проверяем, data это массив или id один, и в зависимости от этого удаляем сообщение или сообщения
+    if isinstance(data, list):
+        for message_id in data:
+            await bot.delete_message(chat_id, message_id)
+    else:
+        await bot.delete_message(chat_id, data)
+
+    # Удаляем сообщение из стейта
+    messages_ids = [ids_dict for ids_dict in messages_ids if model_name not in ids_dict.keys()]
+    logger.info(f"Стейт сообщений по ключу {key} для модели {model_name} после удаления: {messages_ids}")
+    await state.update_data(messages_ids=messages_ids)
---
+++ bot/utils/handlers/getDataInDictsArray.py
@@ -0,0 +1,5 @@
+# Функция для получения данных из массива объектов по имени модели
+async def getDataInDictsArray(array: list[dict], model_name: str):
+    result = next((ids_dict[model_name] for ids_dict in array 
+                                if model_name in ids_dict.keys()), None)
+    return result
