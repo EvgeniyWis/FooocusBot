c3a1cd0183c8b0b60373510d0cd35140b8016519
EvgeniyWis <KworkEvgeniy@yandex.ru>
2025-06-11 21:33:22 +0300

    fix path for deleting temp folder.

--- bot/utils/googleDrive/files/delete_temp_files_with_delay.py
+++ bot/utils/googleDrive/files/delete_temp_files_with_delay.py
@@ -8,6 +8,6 @@ async def delete_temp_files_with_delay(folder_name: str, user_id: int):
     await asyncio.sleep(60 * 60)  # Ждем час
     if not MOCK_MODE:
         try:
-            shutil.rmtree(f"{TEMP_FOLDER_PATH}/{f'{folder_name}_{user_id}' if folder_name else ''}")
+            shutil.rmtree(f"{TEMP_FOLDER_PATH}\\{f'{folder_name}_{user_id}' if folder_name else ''}")
         except Exception as e:
             logger.error(f"Ошибка при удалении временных файлов: {e}")
