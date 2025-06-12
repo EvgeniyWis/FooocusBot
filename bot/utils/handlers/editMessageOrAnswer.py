29df0d58650f6dacc4100440c79d682662142117
EvgeniyWis <KworkEvgeniy@yandex.ru>
2025-06-12 20:21:36 +0300

    Add more safety for editMessageOrAnswer.

--- bot/utils/handlers/editMessageOrAnswer.py
+++ bot/utils/handlers/editMessageOrAnswer.py
@@ -1,15 +1,23 @@
 from aiogram import types
 from logger import logger
+from aiogram.utils.text_decorations import html_decoration
 
 
 # Функция для редактирования сообщения и при случаи ошибки отправки сообщения
 async def editMessageOrAnswer(
         call: types.CallbackQuery, text: str, reply_markup = None):
     try:
-        message = await call.message.edit_text(text, reply_markup=reply_markup)
+        # Экранируем специальные символы в тексте
+        safe_text = html_decoration.quote(text)
+    except Exception as e:
+        logger.warning(f"Ошибка при экранировании текста: {e}")
+        safe_text = text
+    
+    try:
+        message = await call.message.edit_text(safe_text, reply_markup=reply_markup, parse_mode=None)
     except Exception as e:
         logger.warning(f"Ошибка при редактировании сообщения: {e}")
-        message = await call.message.answer(text, reply_markup=reply_markup)
+        message = await call.message.answer(safe_text, reply_markup=reply_markup, parse_mode=None)
 
     return message
 
