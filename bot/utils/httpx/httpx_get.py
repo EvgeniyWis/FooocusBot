827e06bf2916675826dc92dd491de7048875ec6a
EvgeniyWis <KworkEvgeniy@yandex.ru>
2025-06-13 01:14:15 +0300

    Add stream parameter for httpx_get for get only response (not json). And use it in generateVideo to fix it.

--- bot/utils/httpx/httpx_get.py
+++ bot/utils/httpx/httpx_get.py
@@ -3,15 +3,21 @@ from logger import logger
 
 
 # Функция для отправки GET-запросов с настройками таймаутов
-async def httpx_get(url: str, timeout: int = 60):
+async def httpx_get(url: str, headers: dict = None, timeout: int = 60, stream: bool = False):
     async with httpx.AsyncClient(timeout=httpx.Timeout(timeout), follow_redirects=True) as client:
-        response = await client.get(url)
+        response = await client.get(url, headers=headers)
         logger.info(f"Статус ответа: {response.status_code}")
 
         if response.status_code == 200:
-            return response
+            if stream:
+                return response
+            try:
+                response_json = response.json()
+                return response_json
+            except ValueError:
+                return response
 
-        logger.error(f"Неудачный статус ответа: {response.status_code}")
+        logger.error(f"Ошибка при выполнении запроса: {response.status_code}")
         return None
 
 
