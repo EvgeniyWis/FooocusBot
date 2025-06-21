26fbdba9fb4b72d84f002098751e3a8387c8c328
EvgeniyWis <KworkEvgeniy@yandex.ru>
2025-06-21 16:22:48 +0300

    fix (keyboards, state): add callback data personally for quick generation, add new state for it.

--- bot/states/StartGenerationState.py
+++ bot/states/StartGenerationState.py
@@ -1,8 +1,10 @@
 from aiogram.fsm.state import State, StatesGroup
 
+
 class StartGenerationState(StatesGroup):
     write_prompt_for_images = State()
     write_prompt_for_video = State()
+    write_prompt_for_quick_video_generation = State()
     write_prompt_for_model = State()
     write_model_name_for_generation = State()
     send_image_for_video_generation = State()
