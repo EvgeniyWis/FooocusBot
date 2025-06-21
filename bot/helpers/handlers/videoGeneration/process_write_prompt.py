c9e9a02fe021df0fd129c814bb621a0ad4c909b5
EvgeniyWis <KworkEvgeniy@yandex.ru>
2025-06-21 16:21:40 +0300

    decompose (process_write_prompt): split code for writing prompt for video process.

---
+++ bot/helpers/handlers/videoGeneration/process_write_prompt.py
@@ -0,0 +1,42 @@
+from aiogram import types
+from aiogram.fsm.context import FSMContext
+
+from bot.helpers import text
+from bot.helpers.generateImages.dataArray import getModelNameIndex
+from bot.states import StartGenerationState
+from bot.utils.handlers import appendDataToStateArray
+from bot.utils.handlers.messages import editMessageOrAnswer
+
+
+async def process_write_prompt(
+    call: types.CallbackQuery,
+    state: FSMContext,
+    model_name: str,
+    is_quick_generation: bool = False,
+):
+    # Получаем индекс модели
+    model_name_index = getModelNameIndex(model_name)
+
+    await state.update_data(model_name_for_video_generation=model_name)
+    write_prompt_message = await editMessageOrAnswer(
+        call,
+        text.WRITE_PROMPT_FOR_VIDEO_TEXT.format(
+            model_name,
+            model_name_index,
+        ),
+    )
+    # Сохраняем в стейт сообщение о написании промпта для последующего удаления
+    data_for_update = {f"{model_name}": write_prompt_message.message_id}
+    await appendDataToStateArray(
+        state,
+        "write_prompt_messages_ids",
+        data_for_update,
+    )
+
+    # Переключаем стейт
+    if is_quick_generation:
+        await state.set_state(
+            StartGenerationState.write_prompt_for_quick_video_generation,
+        )
+    else:
+        await state.set_state(StartGenerationState.write_prompt_for_video)
