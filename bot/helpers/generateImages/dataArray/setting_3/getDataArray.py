19e6d0447bfb343da222f7cc2d6a77aa1734d0d7
EvgeniyWis <KworkEvgeniy@yandex.ru>
2025-06-29 11:22:30 +0300

    fix two models

--- bot/helpers/generateImages/dataArray/setting_3/getDataArray.py
+++ bot/helpers/generateImages/dataArray/setting_3/getDataArray.py
@@ -9,9 +9,9 @@ def setting3_getDataArray():
             "carolinehazel.xo",
             "1PZHI2_HoKqUGREpUmnEpIb5v9xOKiPLO",
             "1pidIntkPGf_NUP9uu6z17Hh4mlCGB_Rk",
-            """score9, score8up, sourcephoto, sourcereal, hyper-realistic, photorealism, high quality, masterpiece, 8k detail, detailed background, vibrant colors, bright lighting, skin detail, BREAK
+            """score9, high quality, photorealistic, 8k, vibrant colors, bright light, skin detail, BREAK
 
-            realbeauty, igbaddie, 1 girl, 18, sorority girl, long voluminous ash-blonde hair, huge breasts, slim waist, big hips, hourglass figure, toned legs, tan skin, thin lips, smirk, bright blue eyes, sharp cheekbones""",
+            realbeauty, 1 girl, 18, sorority, long ash-blonde hair, big breasts, slim waist, big hips, hourglass, toned legs, tan skin, thin lips, smirk, bright blue eyes, sharp cheekbones""",
             [-1.50, 1.30, 1.00, 3.00, 0.45, -1.00], 8, "score_6, score_5, score_4, Negative_&_Positive_Embeddings_By_Stable_Yogi, negative_hand, pony, negativeXL_D, low quality, oversaturated, disfigured, poorly, bad, wrong, mutated, worst quality, normal quality, ugly face, mutated hands, extra fingers, poorly drawn hands, fused fingers, too many fingers, long neck, bad hands, text, signature, signature artist, multiple female, multiple male, bad anatomy, low res, blurry face, blurry eyes, tiny hands, tiny feet, multiple women, disproportionately large head, disproportionately long torso, six fingers, low quality hands, hat, multicolored hair, pubic hair, asian, tan lines, makeup, lipstick, playing cards, black skin, steering wheel, (man), (((naked nipples))), (((naked breasts))), (((penis))), (((dick))), (((naked pussy))), (((naked))), (((naked ass))), cartoon, anime, 3d, cgi, illustration, doll-like, overly muscular, chubby, plastic skin, waxy texture, blurry, jpeg artifacts, extra limbs, distorted proportions, unnatural face, unrealistic anatomy, deformed eyes, exaggerated curves, barbie face, uncanny valley, big head, overexposed, underexposed, low-quality shading, unnatural smile"),
 
         setting3_generateData("kirapreslyn", "1ig8x6b30H117eTdGRSFHE85G6Dzo6mtk", "1IfPRU1WHAPmJuJW9DnU_D45QysCVurnD", """score_9, score_8_up, score_7_up, source_photo, source_real, hyper-realistic, photorealism, high detailed, high quality, masterpiece, photography, photorealistic, 8k detail, detailed background, ultra-detailed, vibrant colors, bright lighting, skin detail, BREAK
@@ -84,9 +84,9 @@ def setting3_getDataArray():
         realbeauty, 1 girl, 18, sorority girl, long ash-platinum blonde hair, big breasts, slim waist, hourglass figure, toned legs, tan skin, cute face, big lips, smirk, bright blue eyes, sharp cheekbones""",
         [-1.50, 1.30, 1.00, 2.00, 0.30, -0.30]),
 
-        setting3_generateData("callieroux", "1oMwvePG5pSnbquF6wEr6yGWw45EjaeyO", "1u9fE_nQdDVSnbucs95u1kqcn0vwMGRFM", """score9, score8up, sourcephoto, sourcereal, hyper-realistic, photorealism, high quality, masterpiece, 8k detail, detailed background, vibrant colors, bright lighting, skin detail, BREAK
+        setting3_generateData("callieroux", "1oMwvePG5pSnbquF6wEr6yGWw45EjaeyO", "1u9fE_nQdDVSnbucs95u1kqcn0vwMGRFM", """score9, high quality, photorealistic, 8k detail, vibrant colors, bright lighting, skin detail, BREAK
 
-        realbeauty, igbaddie, 1 girl, 18, sorority girl, long voluminous ash-platinum blonde hair, huge breasts, slim waist, big hips, hourglass figure, toned legs, tan skin, thin lips, smirk, bright blue eyes, sharp cheekbones""",
+        realbeauty, 1 girl, 18, sorority girl, long ash-platinum blonde hair, big breasts, slim waist, big hips, hourglass figure, toned legs, tan skin, thin lips, smirk, bright blue eyes, sharp cheekbones""",
         [-1.50, 1.30, 1.00, 3.45, -0.20, 0.30]),
 
         setting3_generateData("aveline_lion", "1ryKfs0xEbcgWw3j35E1hBFsP_vSnuKiD", "1MxgVRtb9qxnZY__yeX1Cj-uEkZQX8Rda", """score_9, score_8_up, score_7_up, source_photo, source_real, hyper-realistic, photorealism, high detailed, high quality, masterpiece, photography, photorealistic, 8k detail, detailed background, ultra-detailed, vibrant colors, bright lighting, skin detail, BREAK
