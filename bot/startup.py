037e27f4b5dff0914019b89e5ce730c165a58beb
EvgeniyWis <KworkEvgeniy@yandex.ru>
2025-07-07 08:59:08 +0300

    code splitting (nsfw video): separate to individual hand file.

--- bot/startup.py
+++ bot/startup.py
@@ -75,6 +75,7 @@ async def on_startup():
     handlers.hand_videoGeneration.hand_add()
     handlers.hand_multi_image.hand_add()
     handlers.hand_img2video.hand_add()
+    handlers.hand_nsfw_video.hand_add()
 
     dp.message.middleware(ErrorHandlingMiddleware())
     dp.callback_query.middleware(ErrorHandlingMiddleware())
