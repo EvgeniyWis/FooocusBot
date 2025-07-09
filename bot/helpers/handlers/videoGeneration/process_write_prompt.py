24892a45375b1493a621c7cd9ff5f70e9ec981f5
EvgeniyWis <KworkEvgeniy@yandex.ru>
2025-07-09 17:57:45 +0300

    fix (img2video): add write prompt message deleting if regeneration.

--- bot/helpers/handlers/videoGeneration/process_write_prompt.py
+++ bot/helpers/handlers/videoGeneration/process_write_prompt.py
@@ -12,7 +12,7 @@ async def process_write_prompt(
     call: types.CallbackQuery,
     state: FSMContext,
     model_name: str,
-    image_index: int,
+    image_index: int | None = None,
     is_quick_generation: bool = False,
     is_nsfw_generation: bool = False,
 ):
@@ -23,7 +23,7 @@ async def process_write_prompt(
         call: CallbackQuery - объект callback-запроса
         state: FSMContext - контекст состояния
         model_name: str - название модели
-        image_index: int - индекс изображения
+        image_index: int | None - индекс изображения
         is_quick_generation: bool - флаг быстрой генерации
         is_nsfw_generation: bool - флаг генерации NSFW
 
@@ -61,17 +61,29 @@ async def process_write_prompt(
         )
 
     # Сохраняем в стейт сообщение о написании промпта для последующего удаления
-    data_for_update = {
-        "model_name": model_name,
-        "image_index": image_index,
-        "message_id": write_prompt_message.message_id,
-    }
-    await appendDataToStateArray(
-        state,
-        "write_prompt_messages_ids",
-        data_for_update,
-        unique_keys=("model_name", "image_index"),
-    )
+    if image_index is not None:
+        data_for_update = {
+            "model_name": model_name,
+            "image_index": image_index,
+            "message_id": write_prompt_message.message_id,
+        }
+        await appendDataToStateArray(
+            state,
+            "write_prompt_messages_ids",
+            data_for_update,
+            unique_keys=("model_name", "image_index"),
+        )
+    else:
+        data_for_update = {
+            "model_name": model_name,
+            "message_id": write_prompt_message.message_id,
+        }
+        await appendDataToStateArray(
+            state,
+            "write_prompt_messages_ids",
+            data_for_update,
+            unique_keys=("model_name"),
+        )
 
     # Переключаем стейт
     if is_quick_generation:
