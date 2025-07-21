94cddc5c8df1cdb1f9827dd62859503273e37108
EvgeniyWis <KworkEvgeniy@yandex.ru>
2025-07-21 17:46:00 +0300

    feat (magnific): add auth get and post.

--- bot/services/freepik/api_client.py
+++ bot/services/freepik/api_client.py
@@ -2,4 +2,22 @@ from bot.services.base_api_client import BaseAPIClient
 
 
 class FreepikAPI(BaseAPIClient):
-    pass
+    def __init__(self, base_url: str, api_key: str) -> None:
+        super().__init__(base_url)
+        self.api_key = api_key
+
+    def _add_auth_header(self, headers=None):
+        auth_header = {
+            "x-freepik-api-key": self.api_key,
+        }
+        if headers:
+            return {**headers, **auth_header}
+        return auth_header
+
+    async def post(self, url, data=None, headers=None, files=None, json=None):
+        headers = self._add_auth_header(headers)
+        return await super().post(url, data=data, headers=headers, files=files, json=json)
+
+    async def get(self, url, headers=None):
+        headers = self._add_auth_header(headers)
+        return await super().get(url, headers=headers)
