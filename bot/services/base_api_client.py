f395229a6c7f81ae39ec546a01083cfcfb3f0e61
EvgeniyWis <KworkEvgeniy@yandex.ru>
2025-07-21 12:00:12 +0300

    refac (api services): add param for base url.

--- bot/services/base_api_client.py
+++ bot/services/base_api_client.py
@@ -9,26 +9,23 @@ class BaseAPIClient:
         self.base_url = base_url
         logger.info(f"Инициализирован API клиент с базовым URL: {base_url}")
 
-    async def post(self, path: str, json: dict = None, files: dict = None) -> dict:
-        url = f"{self.base_url}{path}"
+    async def post(
+        self,
+        path: str,
+        json: dict = None,
+        data: dict = None,
+        files: dict = None,
+        headers: dict = None,
+        with_base_url: bool = True,
+    ) -> dict:
+        url = f"{self.base_url}{path}" if with_base_url else path
         logger.info(f"Отправка POST запроса на {url}")
-        resp = await httpx_post(url, json=json, files=files)
-        try:
-            resp.raise_for_status()
-            logger.info(f"Успешный POST запрос к {path}")
-            return resp.json()
-        except httpx.HTTPError as e:
-            logger.error(f"Ошибка HTTP при POST запросе к {path}: {str(e)}")
-            raise
+        resp = await httpx_post(url, json=json, data=data,
+        files=files, headers=headers)
+        return resp
 
-    async def get(self, path: str) -> dict:
-        url = f"{self.base_url}{path}"
+    async def get(self, path: str, headers: dict = None, with_base_url: bool = True) -> dict:
+        url = f"{self.base_url}{path}" if with_base_url else path
         logger.info(f"Отправка GET запроса на {url}")
-        resp = await httpx_get(url)
-        try:
-            resp.raise_for_status()
-            logger.info(f"Успешный GET запрос к {path}")
-            return resp.json()
-        except httpx.HTTPError as e:
-            logger.error(f"Ошибка HTTP при GET запросе к {path}: {str(e)}")
-            raise
+        resp = await httpx_get(url, headers=headers)
+        return resp
