cab5fede73f9672942bb388f9a59629fa7453ea0
EvgeniyWis <KworkEvgeniy@yandex.ru>
2025-07-22 10:51:26 +0300

    refac (iloveapi): code split logger and validation.

--- bot/services/iloveapi/utils/validation.py
+++ bot/services/iloveapi/utils/validation.py
@@ -1,6 +1,3 @@
-from functools import wraps
-
-from bot.logger import logger
 from bot.services.iloveapi.client.types import FileFormat
 
 
@@ -14,21 +11,3 @@ def validate_file_format(data: FileFormat) -> None:
             raise ValueError(f"Отсутствует обязательное поле '{key}' в FileFormat: {data}")
     # Можно добавить дополнительные проверки типов и значений
 
-
-def log_task_step(step_name: str):
-    """
-    Декоратор для логирования этапов работы с задачей.
-    """
-    def decorator(func):
-        @wraps(func)
-        async def wrapper(*args, **kwargs):
-            logger.info(f"[ILoveAPI] Начало этапа: {step_name}")
-            try:
-                result = await func(*args, **kwargs)
-                logger.info(f"[ILoveAPI] Этап '{step_name}' завершён успешно")
-                return result
-            except Exception as e:
-                logger.error(f"[ILoveAPI] Ошибка на этапе '{step_name}': {e}")
-                raise
-        return wrapper
-    return decorator
